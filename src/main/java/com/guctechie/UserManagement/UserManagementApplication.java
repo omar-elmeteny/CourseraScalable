7e90b3bed7f1ebb5a37f9c1672e4d817f610f2e8
abdelrahman fekri <abdelrahman.fekri@bld.ai>
2024-02-29 20:13:48 +0200

    first commit

---
+++ src/main/java/com/guctechie/UserManagement/UserManagementApplication.java
@@ -0,0 +1,13 @@
+package com.guctechie.UserManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementApplication.class, args);
+	}
+
+}
