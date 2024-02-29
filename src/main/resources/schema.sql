0eb65829a328fe0d32e78312aa57351304a7c495
abdelrahman fekri <abdelrahman.fekri@bld.ai>
2024-02-29 21:24:44 +0200

    feat: adding database schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,7 @@
 
-Create table if not exists 'user' (
+Create database if not exists "usermanagement";
+
+Create table if not exists Users (
     id int(11) not null auto_increment,
     name varchar(255) not null,
     age int(11) not null,
