8693a68c4216d85d0952031b837ae8c5a9fdc1a4
Amer-Ammar <a.ammar@tekunda.com>
2024-03-02 15:35:49 +0200

    refinement

--- src/main/java/com/bugbusters/course/CourseApplication.java
+++ src/main/java/com/bugbusters/course/CourseApplication.java
@@ -2,8 +2,10 @@ package com.bugbusters.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CourseApplication {
     public static void main(String[] args) {
         SpringApplication.run(CourseApplication.class, args);
