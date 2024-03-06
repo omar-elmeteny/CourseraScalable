17191ab68acf180fafdb2f220151450befe13e8d
omar elmeteny <omar.elmeteny@gmail.com>
2024-03-06 18:55:24 +0200

    feat: created data-insertions module

---
+++ data-insertions/src/main/java/com/guctechie/datainsertions/DataInsertionsApplication.java
@@ -0,0 +1,13 @@
+package com.guctechie.datainsertions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataInsertionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataInsertionsApplication.class, args);
+    }
+
+}
---
+++ data-insertions/src/test/java/com/guctechie/datainsertions/DataInsertionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guctechie.datainsertions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataInsertionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
